e217578d42362c6215295ab5fc6ed9e073898d30
Daniel Giribet <dani@calidos.cat>
2019-01-16 00:35:31 +0100

    First end2end Morfeu to Eruinome POST save :yum:

--- src/main/java/cat/calidos/eurinome/control/EurinomeRequestFilter.java
+++ src/main/java/cat/calidos/eurinome/control/EurinomeRequestFilter.java
@@ -1,15 +1,27 @@
 package cat.calidos.eurinome.control;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import cat.calidos.morfeu.problems.FetchingException;
+import cat.calidos.morfeu.utils.Saver;
+import cat.calidos.morfeu.utils.injection.DaggerSaverComponent;
+import cat.calidos.morfeu.utils.injection.DaggerURIComponent;
+import cat.calidos.morfeu.webapp.GenericHttpServlet;
 
 /**
 *	@author daniel giribet
@@ -20,16 +32,46 @@ protected final static Logger log = LoggerFactory.getLogger(EurinomeRequestFilte
 
 @Override
 public void init(FilterConfig filterConfig) throws ServletException {
-
 	log.info("------ Request filter initialized ({}) ------", filterConfig.getFilterName());
-
 }
 
 @Override
 public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 		throws IOException, ServletException {
 
+	HttpServletRequest req = (HttpServletRequest) request;
+
+	log.trace("------ Request filter request ({} {}) ------", req.getMethod(), req.getServletPath());
+	if (req.getMethod().equals("POST")) {
+		ServletContext ctxt = req.getServletContext();
+		String prefix = ctxt.getInitParameter("__RESOURCES_PREFIX");
+		prefix = (String)((Properties)ctxt.getAttribute(GenericHttpServlet.__CONFIG)).get("__RESOURCES_PREFIX");
+		
+		String uri = prefix+req.getServletPath();
+		log.info("------ Request filter req path ({}) ------", uri);
+		
+		// we assume Morfeu is doing the validation for now
+		try {
+			URI destination = DaggerURIComponent.builder().from(uri).builder().uri().get();
+			String content = req.getParameter("content");
+			System.err.println(content);
+			Saver saver = DaggerSaverComponent.builder()
+												.toURI(destination)
+												.content(content)
+												.build()
+												.saver()
+												.get();
+			saver.save();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	} else {
+		chain.doFilter(request, response);
+	}
 }
+
 
 @Override
 public void destroy() {}
